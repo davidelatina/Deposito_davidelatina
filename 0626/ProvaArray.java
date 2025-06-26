38ab2c51e66da141bb6e3228c689d3bbce26499e
Davide Latina <davide.latina616@gmail.com>
2025-06-26 12:27:23 +0200

    Create ProvaArray.java

---
+++ 0626/ProvaArray.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ProvaArray {
+  public static void main(String[] args) {
+    // Dichiarazione
+    Scanner scannerNum = new Scanner(System.in);
+    // non inizializzato
+    int[] numeri = new int[5];
+
+    // inizializzazione diretta
+    int[] valori = {1, 2, 3, 4, 5};
+
+    System.out.println("Primo numero: " + valori[0]);
+
+    // riempimento array
+    System.out.println("Inserire 5 numeri:");
+    for (int i = 0; i < numeri.length; i++) {
+      numeri[i] = scannerNum.nextInt();
+    }
+
+    System.out.print("Numeri inseriti: ");
+    for (int i = 0; i < numeri.length; i++) {
+      System.out.print(numeri[i] + ", ");
+    }
+    System.out.println("");
+
+    scannerNum.close();
+  }
+
+}
