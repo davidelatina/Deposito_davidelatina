2391b7ffb46c17675ab686cee0f6ef371b02da07
Davide Latina <davide.latina616@gmail.com>
2025-06-26 18:53:45 +0200

    Create ProvaArrayList.java

---
+++ 0626/ProvaArrayList.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class ProvaArrayList {
+  public static void main(String[] args) {
+
+    ArrayList<Integer> numeri = new ArrayList<>();
+    numeri.add(10);
+    numeri.add(20);
+    numeri.add(30);
+  }
+  
+}
