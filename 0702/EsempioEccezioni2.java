eea3dc8df9ed2c808d343dd813d533d7bbbee7f3
Davide Latina <davide.latina616@gmail.com>
2025-07-02 10:36:28 +0200

    Create EsempioEccezioni2.java

---
+++ 0702/EsempioEccezioni2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class EsempioEccezioni2 {
+  public static void main(String[] args) {
+    Scanner scanner = null;
+
+    try {
+      scanner = new Scanner(System.in);
+      System.out.println("Inserire un numero: ");
+      int input = scanner.nextInt();
+      System.out.println("Inserito: " + input);
+      int x = input / 0;
+      System.out.println("x = " + x);
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    } finally {
+      scanner.close();
+    }
+  }
+}
