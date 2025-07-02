2e167a720ab66dca1aeceafe296644d57079d389
Davide Latina <davide.latina616@gmail.com>
2025-07-02 11:27:16 +0200

    Create EsempioDivisione.java

---
+++ 0702/EsempioDivisione.java
@@ -0,0 +1,16 @@
+public class EsempioDivisione {
+
+  public static int divisioneChecked(int a, int b) throws Exception {
+    if (b == 0) {
+      throw new Exception("Dividendo 0");
+    }
+    return a/b;
+  }
+
+  public static int divisioneUnchecked(int a, int b) {
+    if (b == 0) {
+      throw new RuntimeException("Dividendo 0");
+    }
+    return a / b;
+  }
+}
