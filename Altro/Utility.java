cd48442bd9b8a747b5453dc01f1da8f24149c065
Davide Latina <davide.latina616@gmail.com>
2025-07-02 22:02:01 +0200

    Cambiamento in classe abstract

--- Altro/Utility.java
+++ Altro/Utility.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
-public class Utility {
-
+public abstract class Utility {
 
   public static void main(String[] args) {
     Scanner scannerNum = new Scanner(System.in);
