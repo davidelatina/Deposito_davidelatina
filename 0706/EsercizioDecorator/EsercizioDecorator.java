4843c3dc5046bf4869bee9dcc68f6162d4f3ba10
Davide Latina <davide.latina616@gmail.com>
2025-07-08 16:19:09 +0200

    Create EsercizioDecorator.java

---
+++ 0706/EsercizioDecorator/EsercizioDecorator.java
@@ -0,0 +1,75 @@
+/*
+Esercizio Facile:
+
+Obiettivo: creare una struttura base del pattern Decorator per decorare un messaggio di
+testo.
+
+Richiesta:
+  Crea un'interfaccia Messaggio con un metodo String getContenuto().
+  Implementa una classe concreta MessaggioBase che restituisce un messaggio semplice.
+  Crea un decoratore DecoratoreMaiuscolo che trasforma il messaggio in maiuscolo.
+  Scrivi una classe Main per mostrare il messaggio decorato a console.
+ */
+
+import java.util.Scanner;
+
+public class EsercizioDecorator {
+  public static void main(String[] args) {
+    Scanner scannerStr = new Scanner(System.in);
+
+    System.out.print("Inserire messaggio: ");
+
+    // Oggetto base
+    MessaggioBase myMessaggioBase = new MessaggioBase(scannerStr.nextLine());
+
+    // Decoratore
+    DecoratoreMaiuscolo myDecoratoreMaiuscolo = new DecoratoreMaiuscolo(myMessaggioBase);
+
+    System.out.println("Messaggio originale: " + myMessaggioBase.getContenuto() + "\nMessaggio decorato: " + myDecoratoreMaiuscolo.getContenuto());
+   
+    scannerStr.close();
+  }
+}
+
+
+// Interfaccia Component
+interface Messaggio {
+  String getContenuto();
+}
+
+// Componente Concreto
+class MessaggioBase implements Messaggio {
+  String messaggio;
+  MessaggioBase(String messaggio) {
+    this.messaggio = messaggio;
+  }
+  public String getContenuto() {
+    return messaggio;
+  }
+}
+
+// Decorator astratto
+abstract class Decorator implements Messaggio {
+  protected Messaggio component;
+
+  public Decorator(Messaggio component) {
+    this.component = component;
+  }
+
+  public String getContenuto() {
+    return component.getContenuto();
+  }
+}
+
+// Decoratore Concreto
+class DecoratoreMaiuscolo extends Decorator {
+  public DecoratoreMaiuscolo(Messaggio component) {
+    super(component);
+  }
+
+  public String getContenuto() { 
+    return component.getContenuto().toUpperCase();
+  }
+}
+
+
