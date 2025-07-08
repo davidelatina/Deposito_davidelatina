64b80d63c376c522684298e604206fe7a7ab59ab
Davide Latina <davide.latina616@gmail.com>
2025-07-08 12:07:59 +0200

    Suddivisione classi in piu file

---
+++ 0706/NotificaBorsa/AgenziaBorsa.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Interfaccia Observer
+interface Investitore_observer {
+  void notifica(String azione, double valore);
+}
+
+// Interfaccia Subject
+interface Subject {
+  void aggiungiInvestitore(RicevitoreNotifiche o);
+
+  void rimuoviInvestitore(RicevitoreNotifiche o);
+
+  void notificaInvestitori();
+}
+
+// Concrete Subject
+class AgenziaBorsa implements Subject {
+  private List<RicevitoreNotifiche> investitori = new ArrayList<>();
+  private String azione;
+  private double valore;
+
+  public void aggiornaValoreAzione(String azione, double valore) {
+    this.azione = azione;
+    this.valore = valore;
+    notificaInvestitori();
+  }
+
+  public void aggiungiInvestitore(RicevitoreNotifiche o) {
+    investitori.add(o);
+  }
+
+  public void rimuoviInvestitore(RicevitoreNotifiche o) {
+   investitori.remove(o);
+  }
+
+  public void notificaInvestitori() {
+    if (investitori.isEmpty()) {
+      return;
+    }
+    for (RicevitoreNotifiche o : investitori) {
+      o.notifica(this.azione, this.valore);
+    }
+  }
+}
+
+class RicevitoreNotifiche implements Investitore_observer {
+  private String name;
+
+  public RicevitoreNotifiche(String name) {
+    this.name = name;
+  }
+   public void notifica(String azione, double valore) {
+     Logger_singleton.getInstance().log(this.toString() + " " + this.name + " ha ricevuto aggiornamento: " + azione + " a € " + valore);
+   }
+}
+
+
+class InvestitorePrivato extends RicevitoreNotifiche {
+
+ public InvestitorePrivato(String name) {
+   super(name);
+ }
+
+}
+
+class InvestitoreBancario extends RicevitoreNotifiche {
+
+ public InvestitoreBancario(String name) {
+   super(name);
+ }
+}
--- 0706/NotificaBorsa/MainBorsa.java
+++ 0706/NotificaBorsa/MainBorsa.java
@@ -17,9 +17,6 @@ Implementare la classe AgenziaBorsa con:
   Implementare almeno due classi Investitore, es. InvestitorePrivato e InvestitoreBancario, che rispondono con messaggi personalizzati.
 */
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MainBorsa {
   public static void main(String[] args) {
     AgenziaBorsa myABorsa = new AgenziaBorsa();
@@ -41,78 +38,7 @@ public class MainBorsa {
     myABorsa.aggiornaValoreAzione("Rockerduck", 1000.0);
     myABorsa.aggiornaValoreAzione("Bassotti Enterprises", 23.0);
 
-
-
   }
  }
 
- // Interfaccia Observer
- interface Investitore_observer {
-   void notifica(String azione, double valore);
- }
-
- // Interfaccia Subject
- interface Subject {
-   void aggiungiInvestitore(RicevitoreNotifiche o);
-
-   void rimuoviInvestitore(RicevitoreNotifiche o);
-
-   void notificaInvestitori();
- }
-
- // Concrete Subject
- class AgenziaBorsa implements Subject {
-   private List<RicevitoreNotifiche> investitori = new ArrayList<>();
-   private String azione;
-   private double valore;
-
-   public void aggiornaValoreAzione(String azione, double valore) {
-     this.azione = azione;
-     this.valore = valore;
-     notificaInvestitori();
-   }
-
-   public void aggiungiInvestitore(RicevitoreNotifiche o) {
-     investitori.add(o);
-   }
-
-   public void rimuoviInvestitore(RicevitoreNotifiche o) {
-    investitori.remove(o);
-   }
-
-   public void notificaInvestitori() {
-     if (investitori.isEmpty()) {
-       return;
-     }
-     for (RicevitoreNotifiche o : investitori) {
-       o.notifica(this.azione, this.valore);
-     }
-   }
- }
-
- class RicevitoreNotifiche implements Investitore_observer {
-   private String name;
-
-   public RicevitoreNotifiche(String name) {
-     this.name = name;
-   }
-    public void notifica(String azione, double valore) {
-      Logger_singleton.getInstance().log(this.toString() + " " + this.name + " ha ricevuto aggiornamento: " + azione + " a € " + valore);
-    }
- }
-
-
- class InvestitorePrivato extends RicevitoreNotifiche {
-
-  public InvestitorePrivato(String name) {
-    super(name);
-  }
-
- }
-
- class InvestitoreBancario extends RicevitoreNotifiche {
-
-  public InvestitoreBancario(String name) {
-    super(name);
-  }
- }
+ 
