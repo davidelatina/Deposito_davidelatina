d5fffd8fb1e9783ea6dc85d0c0da51bb2d90f3bb
Davide Latina <davide.latina616@gmail.com>
2025-07-15 10:32:25 +0200

    Create Facade.java

---
+++ Patterns/Facade.java
@@ -0,0 +1,24 @@
+// Sottosistema
+class SubsystemA {
+  void operationA() {
+    System.out.println("Operazione A del sottosistema A");
+  }
+}
+
+class SubsystemB {
+  void operationB() {
+    System.out.println("Operazione B del sottosistema B");
+  }
+}
+
+// Facade
+class Facade {
+  private SubsystemA a = new SubsystemA();
+  private SubsystemB b = new SubsystemB();
+
+  public void operazioneUnificata() {
+    a.operationA();
+    b.operationB();
+  }
+}
+
