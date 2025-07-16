e5eac48321ce59ba0dcbc1096b0b99c297433600
davidelatina <davide.latina616@gmail.com>
2025-07-16 14:16:11 +0100

    Create EsempioCreateDrop.sql

---
+++ 4-Database/0716/EsempioCreateDrop.sql
@@ -0,0 +1,24 @@
+
+CREATE DATABASE dbtest;
+
+USE dbtest;
+
+CREATE TABLE tabellatest(
+	person_id INT,
+    nome VARCHAR(255),
+    cognome VARCHAR(255),
+    indirizzo VARCHAR(255),
+    citta VARCHAR(255)
+);
+
+CREATE TABLE test2 
+SELECT name, population
+FROM world.city;
+
+SELECT * FROM test2;
+
+DROP TABLE test2;
+
+DROP TABLE tabellatest;
+
+DROP DATABASE dbtest;
