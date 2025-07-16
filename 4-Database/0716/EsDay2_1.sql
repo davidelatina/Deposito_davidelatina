f7f0a66927cafd2a7e8c26f29b33c126c573c2a7
davidelatina <davide.latina616@gmail.com>
2025-07-16 02:11:47 -0700

    Riordinamento esercizio

--- 4-Database/0716/EsDay2_1.sql
+++ 4-Database/0716/EsDay2_1.sql
@@ -1,7 +1,30 @@
 USE world;
 
--- lingue parlate per nazione, con percentuale di utilizzo
+-- 1 lingue parlate per nazione, con percentuale di utilizzo
 SELECT country.name, countrylanguage.language, countrylanguage.Percentage
 FROM country, countrylanguage
 WHERE country.Code = countrylanguage.countrycode
 ORDER BY name ASC, Percentage DESC;
+
+-- 2 lista nazioni, con percentuale della lingua più parlata
+SELECT country.name, countrylanguage.language, countrylanguage.Percentage
+FROM country, countrylanguage
+WHERE country.Code = countrylanguage.countrycode
+AND percentage >= ALL (
+	SELECT countrylanguage.Percentage
+	FROM countrylanguage
+	WHERE country.Code = countrylanguage.countrycode
+)
+ORDER BY name ASC;
+
+
+-- 3 lingua più parlata di una nazione, con percentuale
+SELECT country.name, countrylanguage.language, countrylanguage.Percentage
+FROM country, countrylanguage
+WHERE country.name = 'Italy'
+AND country.Code = countrylanguage.countrycode
+AND percentage >= ALL (
+	SELECT countrylanguage.Percentage
+	FROM countrylanguage
+	WHERE country.Code = countrylanguage.countrycode
+);
