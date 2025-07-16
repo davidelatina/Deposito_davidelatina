373e9ea81af095ec89dcab81bbfb3293084be377
davidelatina <davide.latina616@gmail.com>
2025-07-16 13:42:44 +0100

    creazione progetto test per jdbc

---
+++ 4-Database/ProgettoJDBC/demo/src/main/java/com/progettojdbc/Main.java
@@ -0,0 +1,18 @@
+package com.progettojdbc;
+
+import java.sql.*;
+        
+public class Main {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/"; // sostituire con il nome del DB
+        String user = "root";       // utente del DB
+        String password = "dmty@Z'7(=x6";       // password del DB
+
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Connessione riuscita");
+        } catch (SQLException e) {
+            System.out.println("Errore nella connessione");
+            e.printStackTrace();
+        }
+    }
+}
