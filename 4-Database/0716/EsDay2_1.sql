399b82cd36d5c9ff73daa8d1aa722a87cef6a4ae
davidelatina <davide.latina616@gmail.com>
2025-07-16 02:29:20 -0700

    Update EsDay2_1.sql

--- 4-Database/0716/EsDay2_1.sql
+++ 4-Database/0716/EsDay2_1.sql
@@ -6,6 +6,13 @@ FROM country, countrylanguage
 WHERE country.Code = countrylanguage.countrycode
 ORDER BY name ASC, Percentage DESC;
 
+-- con JOIN
+SELECT country.name, countrylanguage.language, countrylanguage.Percentage
+FROM country
+JOIN countrylanguage ON country.Code = countrylanguage.countrycode
+ORDER BY name ASC, Percentage DESC;
+
+
 -- 2 lista nazioni, con percentuale della lingua più parlata
 SELECT country.name, countrylanguage.language, countrylanguage.Percentage
 FROM country, countrylanguage
@@ -16,6 +23,16 @@ AND percentage >= ALL (
 	WHERE country.Code = countrylanguage.countrycode
 )
 ORDER BY name ASC;
+
+-- con JOIN
+SELECT country.name, countrylanguage.language, countrylanguage.Percentage
+FROM country
+JOIN countrylanguage ON country.Code = countrylanguage.countrycode
+WHERE percentage >= ALL (
+	SELECT countrylanguage.Percentage
+	FROM countrylanguage
+	WHERE country.Code = countrylanguage.countrycode
+);
 
 
 -- 3 lingua più parlata di una nazione, con percentuale
@@ -28,3 +45,19 @@ AND percentage >= ALL (
 	FROM countrylanguage
 	WHERE country.Code = countrylanguage.countrycode
 );
+
+-- con JOIN
+SELECT paese, lingua, percentuale
+FROM (
+	SELECT country.name AS paese, countrylanguage.language AS lingua, countrylanguage.Percentage AS percentuale
+	FROM country
+	JOIN countrylanguage ON country.Code = countrylanguage.countrycode
+) AS sublingue
+WHERE percentuale >= ALL (
+	SELECT countrylanguage.percentage
+    FROM countrylanguage
+    WHERE countrylanguage.CountryCode = (
+		SELECT code FROM country WHERE name = sublingue.paese
+	)
+);
+
